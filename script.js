b2f5f32d1ec466974ab44bdf0f67214994c20eff
Jan Ferdie Almario <namikazehaze@gmail.com>
2026-05-16 10:16:37 +0800

    add decent profile - add image for js - add new nav bar and hamburger - fix media queries

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-            const modal = document.getElementById("imageModal");
-            const modalImg = document.getElementById("modalImg");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-            function openModal(src) {
-            modal.classList.remove("hidden");
-            modalImg.src = src;
-            }
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
 
-            modal.addEventListener("click", () => {
-            modal.classList.add("hidden");
-            });
+document.querySelectorAll(".nav-link").forEach(n =>
+    n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    })
+);
+
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImg");
+
+    function openModal(src) {
+    modal.classList.remove("hidden");
+    modalImg.src = src;
+    }
+
+    modal.addEventListener("click", () => {
+    modal.classList.add("hidden");
+ });
